a00f6c751412b4056dbd0c0c0ab55ea1daa0987a
“SamedHira626” <samedhira626@gmail.com>
2020-03-22 03:15:25 +0300

    Roll A Ball

---
+++ Assets/Codes/BallControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    Rigidbody physics;
+    public int speed;
+    int count = 0;
+    public int collectingCoins;
+    public Text counterText;
+    public Text youDidText;
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+        physics.AddForce(vec*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "block")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            counterText.text = "Count = " + count;
+            if(count == collectingCoins)
+            {
+                youDidText.text = "You did !";
+            }
+        }
+    }
+
+}
---
+++ Assets/Codes/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 distance;
+    void Start()
+    {
+        distance = transform.position - ball.transform.position; //to follow the ball for camera
+    }
+
+    void Update()
+    {
+        transform.position = ball.transform.position + distance;
+    }
+}
